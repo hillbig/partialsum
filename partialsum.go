3cf88fb13c43a75b382486919cf3bc000ae2d3aa
Daisuke Okanohara <hillbig@preferred.jp>
2014-10-13 07:16:38 +0900

    support MarshalBinary, UnmarshalBinary

--- partialsum.go
+++ partialsum.go
@@ -2,6 +2,7 @@ package partialsum
 
 import (
 	"github.com/hillbig/rsdic"
+	"github.com/ugorji/go/codec"
 )
 
 // PartialSum stores non-negative integers V[0...N)
@@ -28,24 +29,30 @@ type PartialSum interface {
 	LookupAndSum(ind uint64) (val uint64, sum uint64)
 
 	// Find returns ind satisfying Sum(ind) <= val < Sum(ind+1)
-	// and val - Sum(ind). If there are multiple ind
-	// satisfy this condition, return the minimum one.
+	// and val - Sum(ind). If there are multiple inds
+	// satisfiying this condition, return the minimum one.
 	Find(val uint64) (ind uint64, offset uint64)
+
+	// MarshalBinary encodes VecString into a binary form and returns the result.
+	MarshalBinary() ([]byte, error)
+
+	// UnmarshalBinary decodes the FixVec form a binary from generated MarshalBinary
+	UnmarshalBinary([]byte) error
 }
 
 func New() PartialSum {
 	dic := rsdic.New()
 	dic.PushBack(true)
-	return &PartialSumImpl{
+	return &partialSumImpl{
 		dic: dic,
 	}
 }
 
-type PartialSumImpl struct {
+type partialSumImpl struct {
 	dic rsdic.RSDic
 }
 
-func (ps *PartialSumImpl) IncTail(ind uint64, val uint64) {
+func (ps *partialSumImpl) IncTail(ind uint64, val uint64) {
 	for i := ps.dic.OneNum(); i <= ind; i++ {
 		ps.dic.PushBack(true)
 	}
@@ -54,32 +61,52 @@ func (ps *PartialSumImpl) IncTail(ind uint64, val uint64) {
 	}
 }
 
-func (ps PartialSumImpl) Num() uint64 {
+func (ps partialSumImpl) Num() uint64 {
 	return ps.dic.OneNum()
 }
 
-func (ps PartialSumImpl) AllSum() uint64 {
+func (ps partialSumImpl) AllSum() uint64 {
 	return ps.dic.ZeroNum()
 }
 
-func (ps PartialSumImpl) Lookup(ind uint64) (val uint64) {
+func (ps partialSumImpl) Lookup(ind uint64) (val uint64) {
 	return ps.dic.Select(ind+1, true) - ps.dic.Select(ind, true) - 1
 }
 
-func (ps PartialSumImpl) Sum(ind uint64) (sum uint64) {
+func (ps partialSumImpl) Sum(ind uint64) (sum uint64) {
 	return ps.dic.Rank(ps.dic.Select(ind, true), false)
 }
 
-func (ps PartialSumImpl) LookupAndSum(ind uint64) (val uint64, sum uint64) {
+func (ps partialSumImpl) LookupAndSum(ind uint64) (val uint64, sum uint64) {
 	indPos := ps.dic.Select(ind, true)
 	sum = ps.dic.Rank(indPos, false)
 	val = ps.dic.Select(ind+1, true) - indPos - 1
 	return
 }
 
-func (ps PartialSumImpl) Find(val uint64) (ind uint64, offset uint64) {
+func (ps partialSumImpl) Find(val uint64) (ind uint64, offset uint64) {
 	pos := ps.dic.Select(val, false)
 	ind = ps.dic.Rank(pos, true) - 1
 	offset = pos - ps.dic.Select(ind, true) - 1
+	return
+}
+
+func (ps partialSumImpl) MarshalBinary() (out []byte, err error) {
+	var mh codec.MsgpackHandle
+	enc := codec.NewEncoderBytes(&out, &mh)
+	err = enc.Encode(ps.dic)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (ps *partialSumImpl) UnmarshalBinary(in []byte) (err error) {
+	var mh codec.MsgpackHandle
+	dec := codec.NewDecoderBytes(in, &mh)
+	err = dec.Decode(&ps.dic)
+	if err != nil {
+		return
+	}
 	return
 }
